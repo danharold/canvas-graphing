efde9a4140ceb649af9c73c4cde02b802439d74d
Dan Harold <dan.harold2001@hotmail.com>
2024-03-05 18:37:58 +0000

    transform manager for updating viewtransform

--- src/components/Graph.ts
+++ src/components/Graph.ts
@@ -1,87 +1,17 @@
-import { getMousePos } from '../utils/inputHandler';
-import {
-	Position,
-	ViewTransform,
-	screenToPos,
-	worldToPos
-} from '../utils/graphicsUtils';
 import { canvas, ctx } from '../utils/CanvasContextManager';
+import { CanvasTransformManager } from '../utils/CanvasTransformManager';
+import { Position, worldToPos } from '../utils/graphicsUtils';
 import Square from './Square';
 
 export default class Graph {
-	clicked: boolean = false;
-	mouseClickPos: Position = {
-		world: { x: 0, y: 0 },
-		screen: { x: 0, y: 0 }
-	};
-	currentMousePos: Position = {
-		world: { x: 0, y: 0 },
-		screen: { x: 0, y: 0 }
-	};
-	vt: ViewTransform = {
-		offset: { x: 0, y: 0 },
-		scale: 1
-	};
-
+	transform: CanvasTransformManager;
 	squares: Square[] = [];
 
 	constructor() {
 		canvas.style.backgroundColor = 'white';
-		window.addEventListener('resize', () => this.resizeCanvas());
-		window.addEventListener('mousedown', (e) => this.toggleDown(e));
-		window.addEventListener('mouseup', (e) => this.toggleUp(e));
-		window.addEventListener('mousemove', (e) => this.pan(e));
+		this.transform = new CanvasTransformManager();
 		window.addEventListener('keydown', (e) => this.spawnSquare(e));
-		window.addEventListener('wheel', (e) => this.zoom(e));
-		this.resizeCanvas();
 		this.update();
-	}
-
-	// resize canvas to full screen and redraw
-	resizeCanvas(): void {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-	}
-
-	// if mouse clicked
-	// set clicked to true and record click location
-	toggleDown(e: MouseEvent) {
-		this.clicked = true;
-		this.mouseClickPos = getMousePos(e, this.vt);
-	}
-
-	// if mouse is not clicked, set clicked to false
-	// panning stops
-	toggleUp(e: MouseEvent) {
-		this.clicked = false;
-	}
-
-	// update current mouse location while moving
-	// and if mouse moves while clicked,
-	// then pan, which means update the offset and draw
-	// this means the canvas is static when no panning is occurring
-	pan(e: MouseEvent) {
-		this.currentMousePos = getMousePos(e, this.vt);
-		if (this.clicked) {
-			this.vt.offset.x +=
-				this.currentMousePos.world.x - this.mouseClickPos.world.x;
-			this.vt.offset.y +=
-				this.currentMousePos.world.y - this.mouseClickPos.world.y;
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-		}
-	}
-
-	zoom(e: WheelEvent) {
-		if (e.deltaY > 0) {
-			this.vt.scale *= 1 / 1.2;
-		} else if (e.deltaY < 0) {
-			this.vt.scale *= 1.2;
-		}
-		let mousePosAfterZoom = screenToPos(this.currentMousePos.screen, this.vt);
-		this.vt.offset.x +=
-			mousePosAfterZoom.world.x - this.currentMousePos.world.x;
-		this.vt.offset.y +=
-			mousePosAfterZoom.world.y - this.currentMousePos.world.y;
 	}
 
 	// spawn square on mouse when pressing E
@@ -91,21 +21,31 @@ export default class Graph {
 			let randomHeight = Math.random() * 100;
 			let colour = 'black';
 			let sq = new Square(
-				this.currentMousePos,
+				this.transform.currentMousePos,
 				randomWidth,
 				randomHeight,
 				colour
 			);
 			this.squares.push(sq);
+			console.log(sq.pos.world);
 		}
 	}
 
 	draw(): void {
 		for (let i = 0; i < this.squares.length; i++) {
-			this.squares[i].draw(this.vt);
+			this.squares[i].draw(this.transform.vt);
 		}
 		ctx.fillStyle = 'black';
-		ctx.fillRect(10, 10, 100, 100);
+		ctx.font = '50px roman';
+		let test = {
+			'(+x,+y)': worldToPos({ x: 250, y: 250 }, this.transform.vt),
+			'(-x,+y)': worldToPos({ x: -250, y: 250 }, this.transform.vt),
+			'(+x,-y)': worldToPos({ x: 250, y: -250 }, this.transform.vt),
+			'(-x,-y)': worldToPos({ x: -250, y: -250 }, this.transform.vt)
+		};
+		for (const [key, value] of Object.entries(test)) {
+			ctx.fillText(key, value.screen.x, value.screen.y);
+		}
 	}
 
 	update() {
---
+++ src/utils/CanvasTransformManager.ts
@@ -0,0 +1,91 @@
+import {
+	Position,
+	ViewTransform,
+	Vector2D,
+	screenToPos
+} from './graphicsUtils';
+import { canvas, ctx } from './CanvasContextManager';
+
+export class CanvasTransformManager {
+	vt: ViewTransform = {
+		offset: { x: 0, y: 0 },
+		scale: 1
+	};
+	clicked: boolean = false;
+	mouseClickPos: Position = {
+		world: { x: 0, y: 0 },
+		screen: { x: 0, y: 0 }
+	};
+	currentMousePos: Position = {
+		world: { x: 0, y: 0 },
+		screen: { x: 0, y: 0 }
+	};
+
+	constructor() {
+		window.addEventListener('resize', () => this.resizeCanvas());
+		window.addEventListener('mousedown', (e) => this.toggleDown(e));
+		window.addEventListener('mouseup', (e) => this.toggleUp(e));
+		window.addEventListener('mousemove', (e) => this.pan(e));
+		window.addEventListener('wheel', (e) => this.zoom(e));
+		this.resizeCanvas();
+		this.vt.offset = {
+			x: canvas.width / 2,
+			y: canvas.height / 2
+		};
+	}
+
+	// resize canvas to full screen and redraw
+	private resizeCanvas(): void {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	}
+
+	// if mouse clicked
+	// set clicked to true and record click location
+	private toggleDown(e: MouseEvent): void {
+		this.clicked = true;
+		this.mouseClickPos = this.getMousePos(e);
+	}
+
+	// if mouse is not clicked, set clicked to false
+	// panning stops
+	private toggleUp(e: MouseEvent): void {
+		this.clicked = false;
+	}
+
+	// update current mouse location while moving
+	// and if mouse moves while clicked,
+	// then pan, which means update the offset and draw
+	// this means the canvas is static when no panning is occurring
+	private pan(e: MouseEvent): void {
+		this.currentMousePos = this.getMousePos(e);
+		if (this.clicked) {
+			this.vt.offset.x +=
+				this.currentMousePos.world.x - this.mouseClickPos.world.x;
+			this.vt.offset.y +=
+				this.currentMousePos.world.y - this.mouseClickPos.world.y;
+		}
+	}
+
+	private zoom(e: WheelEvent): void {
+		if (e.deltaY > 0) {
+			this.vt.scale *= 1 / 1.2;
+		} else if (e.deltaY < 0) {
+			this.vt.scale *= 1.2;
+		}
+		let mousePosAfterZoom = screenToPos(this.currentMousePos.screen, this.vt);
+		this.vt.offset.x +=
+			mousePosAfterZoom.world.x - this.currentMousePos.world.x;
+		this.vt.offset.y +=
+			mousePosAfterZoom.world.y - this.currentMousePos.world.y;
+	}
+
+	private getMousePos(event: MouseEvent): Position {
+		let rect = canvas.getBoundingClientRect();
+		let screenPos: Vector2D = {
+			x: event.clientX - rect.left,
+			y: event.clientY - rect.top
+		};
+		return screenToPos(screenPos, this.vt);
+	}
+}
