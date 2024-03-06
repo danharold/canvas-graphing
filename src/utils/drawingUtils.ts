316701008a8a29be55c848ff062dcd338c931dca
Dan Harold <dan.harold2001@hotmail.com>
2024-03-06 10:03:04 +0000

    plot point

--- src/utils/drawingUtils.ts
+++ src/utils/drawingUtils.ts
@@ -25,3 +25,33 @@ export function plotLine(
 	ctx.stroke();
 	ctx.restore();
 }
+
+// plot a point
+export function plotPoint(
+	center: Vector2D,
+	r: number,
+	colour: string,
+	vt: ViewTransform,
+	outline: boolean = false,
+	outlineWidth: number = 1,
+	outlineColour: string = 'white'
+) {
+	const centerPos = worldToPos(center, vt).screen;
+	ctx.save();
+	if (outline) {
+		ctx.beginPath();
+		ctx.arc(centerPos.x, centerPos.y, r, 0, 2 * Math.PI);
+		ctx.fillStyle = outlineColour;
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(centerPos.x, centerPos.y, r - outlineWidth, 0, 2 * Math.PI);
+		ctx.fillStyle = colour;
+		ctx.fill();
+	} else {
+		ctx.beginPath();
+		ctx.arc(centerPos.x, centerPos.y, r, 0, 2 * Math.PI);
+		ctx.fillStyle = colour;
+		ctx.fill();
+	}
+	ctx.restore();
+}
--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -3,9 +3,9 @@ import { CanvasTransformManager } from './CanvasTransformManager';
 export function updateDebugInfo(transform: CanvasTransformManager): void {
 	const ui = document.getElementById('ui') as HTMLElement;
 	ui.innerHTML = `
-        <strong>mouse pos</strong><br>
-        world: ${round(transform.currentMousePos.world.x, 2)}, ${round(transform.currentMousePos.world.y, 2)}<br>
-        screen: ${round(transform.currentMousePos.screen.x, 2)}, ${round(transform.currentMousePos.screen.y, 2)}<br><br>
+        <strong>mouse position</strong><br>
+        &nbsp; world: ${round(transform.currentMousePos.world.x, 2)}, ${round(transform.currentMousePos.world.y, 2)}<br>
+        &nbsp; screen: ${round(transform.currentMousePos.screen.x, 2)}, ${round(transform.currentMousePos.screen.y, 2)}<br><br>
         <strong>view transform</strong><br>
         &nbsp; offset: ${round(transform.vt.offset.x, 2)}, ${round(transform.vt.offset.y, 2)}<br>
         &nbsp; scale: ${round(transform.vt.scale, 2)}<br>
