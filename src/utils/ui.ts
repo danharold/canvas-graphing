8fa01bb335a3cc99eece5e178ae01e043b14b0e5
danharold <dan.harold2001@hotmail.com>
2024-03-26 17:24:37 +0000

    buttons open a drawer with input fields

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -2,6 +2,7 @@ import { Drawable } from '../components/models';
 import { CanvasTransformManager } from './CanvasTransformManager';
 import { getWorldBoundary } from './utils';
 
+// toggle class to open/close debug info
 const debugToggle = document.querySelector('.debug-ui-toggle') as HTMLElement;
 const debugInfo = document.querySelector('.debug-ui-info') as HTMLElement;
 
@@ -9,12 +10,97 @@ debugToggle.addEventListener('click', () => {
 	debugInfo.classList.toggle('debug-ui--open');
 });
 
+// toggle class to open/close add plot menu
+const addUI = document.querySelector('.add-ui') as HTMLElement;
 const addToggle = document.querySelector('.add-ui-toggle') as HTMLElement;
 const addMenu = document.querySelector('.add-ui-menu') as HTMLElement;
 
 addToggle.addEventListener('click', () => {
 	addMenu.classList.toggle('add-ui--open');
 });
+
+// function to create buttons for several plotting options
+// also on click, open a drawer with relevant inputFields
+interface Option {
+	name: string;
+	inputFields: string[];
+}
+function createPlotMenu(plotOptions: Option[]): void {
+	plotOptions.forEach((option) => {
+		const button = document.createElement('p');
+		button.textContent = option.name;
+		button.addEventListener('click', () => {
+			createDrawer(option, button);
+		});
+		addMenu.appendChild(button);
+	});
+}
+
+// initialise the menu
+const plotOptions: Option[] = [
+	{ name: 'point', inputFields: [] },
+	{ name: 'linear', inputFields: ['m', 'c'] },
+	{ name: 'blablabla', inputFields: [] },
+	{ name: 'trigonometric', inputFields: [] }
+];
+createPlotMenu(plotOptions);
+
+// create a drawer given a plot option, allowing relevant inputs
+function createDrawer(option: Option, button: HTMLElement) {
+	const addMenu = document.querySelector('.add-ui-menu') as HTMLElement;
+
+	// if an existing drawer exists, remove it
+	const existingDrawer = document.querySelector('.drawer');
+	if (existingDrawer) {
+		existingDrawer.classList.remove('open');
+		// remove element after transition is over
+		existingDrawer.addEventListener('transitionend', () => {
+			existingDrawer.remove();
+		});
+		if (existingDrawer.id === option.name + '-drawer') {
+			// the current button was clicked to close the drawer, so return out
+			return;
+		}
+	}
+
+	// initialise drawer div with unique id for checking if an opened drawer was clicked to be closed
+	const drawer = document.createElement('div');
+	drawer.classList.add('drawer');
+	drawer.id = option.name + '-drawer';
+
+	// input fields
+	option.inputFields.forEach((input) => {
+		const inputField = document.createElement('input');
+		inputField.type = 'number';
+		inputField.placeholder = input;
+		inputField.id = input + 'Input';
+
+		// match against regex to only allow numeric inputs
+		inputField.addEventListener('input', () => {
+			const regex = /^[+-]?(\d*\.)?\d+$/; // Regex to match numbers and floats
+			if (!regex.test(inputField.value)) {
+				inputField.value = inputField.value.replace(/[^0-9.-]/g, '');
+			}
+		});
+
+		drawer.appendChild(inputField);
+	});
+
+	// add plot button
+	const plotButton = document.createElement('div');
+	plotButton.textContent = 'add';
+	plotButton.addEventListener('click', () => {
+		console.log('addplot', option);
+	});
+	drawer.appendChild(plotButton);
+
+	// insert the drawer element after the button (not as a child)
+	button.insertAdjacentElement('afterend', drawer);
+
+	setTimeout(() => {
+		drawer.classList.add('open');
+	}, 10);
+}
 
 export function updateDebugInfo(
 	transform: CanvasTransformManager,
